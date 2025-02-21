6225391744900596390da9fdd375f6494c8cafc6
Matheus Lima <matheus.slima@sptech.school>
2025-02-21 10:09:41 -0300

    commit inicial da api da barbearia

---
+++ src/main/java/org/example/apibarbeariamanodi/ApiBarbeariamanodiApplication.java
@@ -0,0 +1,13 @@
+package org.example.apibarbeariamanodi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "org.example.apibarbeariamanodi")
+public class ApiBarbeariamanodiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBarbeariamanodiApplication.class, args);
+    }
+
+}
